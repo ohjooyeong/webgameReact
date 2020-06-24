c241094060698b14038fd4d96098249f61c74cb1
OH-JOO-YEONG <brb1111@naver.com>
2020-06-24 23:23:18 +0900

    GuGudan webpack apply

--- 1.GuGudan/webpack.config.js
+++ 1.GuGudan/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     name: "Gugudan-setting",
@@ -18,13 +19,24 @@ module.exports = {
                 test: /\.jsx?$/,
                 loader: "babel-loader",
                 options: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    presets: [
+                        [
+                            "@babel/preset-env",
+                            {
+                                targets: {
+                                    // preset의 플러그인 중 지난 브라우저 적용 플러그인
+                                    browsers: ["> 5% in KR"], // browserslist
+                                },
+                            },
+                        ],
+                        "@babel/preset-react",
+                    ],
                     plugins: [],
                 },
             },
         ],
     },
-
+    plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
     output: {
         path: path.join(__dirname, "dist"),
         filename: "app.js",
