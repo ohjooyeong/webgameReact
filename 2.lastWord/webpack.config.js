6bc635bf0f5fdf19ab234e8a1fa09c4e50bf0888
OH-JOO-YEONG <brb1111@naver.com>
2020-06-25 23:42:25 +0900

    webpack apply React-hot-loader

--- 2.lastWord/webpack.config.js
+++ 2.lastWord/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                         ],
                         "@babel/preset-react",
                     ],
-                    plugins: ["@babel/plugin-proposal-class-properties"],
+                    plugins: ["@babel/plugin-proposal-class-properties", "react-hot-loader/babel"],
                 },
             },
         ],
@@ -40,5 +40,6 @@ module.exports = {
     output: {
         path: path.join(__dirname, "dist"),
         filename: "app.js",
+        publicPath: "/dist/",
     }, // 출력
 };
